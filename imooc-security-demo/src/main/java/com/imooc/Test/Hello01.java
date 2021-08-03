53dc1609140682b3972d5c9018f26cc5c9bc6e0e
Diana <zhoudingjing123@gmail.com>
2021-08-03 22:32:46 +0800

    新建的一个类

---
+++ imooc-security-demo/src/main/java/com/imooc/Test/Hello01.java
@@ -0,0 +1,15 @@
+package com.imooc.Test;
+
+/**
+ * @Author: DongXin
+ * @Description:
+ * @Date:
+ */
+public class Hello01 {
+    public static void main(String[] args) {
+        System.out.println("您好");
+        System.out.println("您好");
+        System.out.println("您好");
+        System.out.println("您好");
+    }
+}

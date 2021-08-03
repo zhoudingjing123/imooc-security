dbb9a81966517a8abe9bf90111fa07a2c5a5c36b
Zhoujing <zj246141807@163.com>
2021-08-03 11:17:21 +0800

    测试仓库创新

---
+++ imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: DongXin
+ * @Description:
+ * @Date:
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}

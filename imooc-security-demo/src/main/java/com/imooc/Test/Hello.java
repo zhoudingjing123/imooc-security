b2e0d0824d0c3f23c6e28707bc0b27ef8e38a495
Diana <zhoudingjing123@gmail.com>
2021-08-03 14:49:51 +0800

    类提交

--- imooc-security-demo/src/main/java/com/imooc/Test/Hello.java
+++ imooc-security-demo/src/main/java/com/imooc/Test/Hello.java
@@ -1,9 +1,12 @@
-package com.imooc;
+package com.imooc.Test;
 
 /**
  * @Author: DongXin
  * @Description:
  * @Date:
  */
-public class Helloworld {
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("ninhao");
+    }
 }
